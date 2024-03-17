b5dc2c31dae7536f871f77d5622e2d7cd1cd437a
Bubblesz Hernandez <bubbleszhernandez@Hearts-MacBook-Pro.local>
2024-03-17 11:53:15 -0400

    “UPDATED.”

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -10,10 +10,10 @@ const Character = ({ character }) => {
 
   return (
     <div className="character-card" onClick={toggleHomeworld}>
-      <h3>{character.name}</h3>
+      <h3 className="character-name">{character.name}</h3> {/* Apply class name for character name */}
       <p>ID: {character.id}</p>
       <p>Date of Birth: {character.birth_year}</p>
-      {showHomeworld && <p>Home World: {character.homeworld.name}</p>}
+      {showHomeworld && <p className="character-planet">Home World: {character.homeworld.name}</p>} {/* Apply class name for character planet */}
     </div>
   );
 };
